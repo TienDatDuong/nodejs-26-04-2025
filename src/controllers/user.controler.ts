767b373b6252af08ee28e712946028310a2039f9
DESKTOP-P49PQHR\Pc <duongtiendat3103@gmail.com>
2025-04-30 18:10:59 +0700

    create form

--- src/controllers/user.controler.ts
+++ src/controllers/user.controler.ts
@@ -1,3 +1,4 @@
+import { log } from 'console';
 import {Request,Response} from 'express'
 
 const getHomePage = (req: Request,res: Response) => {
@@ -8,4 +9,12 @@ const getCreateUserPage = (req: Request,res: Response) => {
     res.render('create-user');
 }
 
-export {getHomePage,getCreateUserPage}
+const postCreayteUser = (req: Request,res: Response) => {
+    console.log(req)
+    // const {firstName,lastName} = req.body
+    // console.log(firstName,lastName)
+    // res.send("User created successfully")
+}
+
+
+export {getHomePage,getCreateUserPage, postCreayteUser}
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,11 +1,13 @@
 import express,{Express} from 'express'
-import { getHomePage ,getCreateUserPage} from '../controllers/user.controler'
+import { getHomePage ,getCreateUserPage, postCreayteUser} from '../controllers/user.controler'
 const router = express.Router()
 
 const webRouter = (app: Express) => {
     app.get("/",getHomePage)
 
     app.get("/create-user",getCreateUserPage)
+
+    app.post("/handle-create-user", postCreayteUser)
 
     app.use("/",router)
 }
