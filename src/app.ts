c9cf2010fd83960a35b66bdbfab1ffc2e82d9999
Duong Tien Dat <duongtiendat3103@gmail.com>
2025-05-08 10:57:11 +0700

    add a new user with prisma

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // const express = require('express');
 import  express  from "express";
 import webRouter from "./routes/web"
-import getConnection from "./db";
+import getConnection from "./config/db";
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
